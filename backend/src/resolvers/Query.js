7d8d1a009aeecd88fcd402d543ea35cb7ef3c1e7
Marcos Lessa <mlessa@gmail.com>
2018-12-27 09:58:35 +0000

    implement update item

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db'),
   // async items(parents, args, ctx, info){
   //   const items = await ctx.db.query.item();
   //   return items;
