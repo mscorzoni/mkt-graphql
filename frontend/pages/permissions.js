a9298611627a05608d1ff15d6c09adf6cd274aca
Marcos Lessa <mlessa@gmail.com>
2018-12-31 18:08:27 +0000

    create permissions page

---
+++ frontend/pages/permissions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PleaseSignIn from '../components/PleaseSignIn';
+import Permissions from '../components/Permissions';
+
+const PermissionsPage = props => {
+  return (
+    <div>
+      <PleaseSignIn>
+        <Permissions />
+      </PleaseSignIn>
+    </div>
+  );
+};
+
+export default PermissionsPage;
