20f9e54b46eb664f7fc1708a6b5ef64f8b446b8e
Marcos Lessa <mlessa@gmail.com>
2018-12-31 15:59:48 +0000

    implement send email to reset token

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -96,6 +97,18 @@ const Mutations = {
     where: { email: args.email },
     data: { resetToken, resetTokenExpiry }
    });
+   const mailRes = await transport.sendMail({
+     from: 'mlessa@gmail.com',
+     to: user.email,
+     subject: 'Your Password Reset Token',
+     html: makeANiceEmail(`Your Password Reset Token is here
+     \n\n
+     <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">
+     Click Here to Reset 
+     </a>
+     `)
+   });
+
    return { message: 'Thanks' };
   },
   async resetPassword(parent, args, ctx, info) {
