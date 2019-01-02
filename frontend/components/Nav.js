bca9e82adc9f35b186fe87707529a6f6d9eb3a4f
Marcos Lessa <mlessa@gmail.com>
2019-01-02 12:16:01 +0000

    add local state to cart drawer

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -24,6 +26,11 @@ const Nav = () => {
                 <a>account</a>
               </Link>
               <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION} >
+                {(toggleCart) => (
+                  <button onClick={toggleCart}>My cart</button>
+                )}
+              </Mutation>  
             </>
           )}
           {!me && (
